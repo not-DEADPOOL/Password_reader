57bd654b7cb8a8e3e5a31670fd45c44be8098c98
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2022-10-13 11:36:47 +0200

    Add files via upload

---
+++ es_io_3/es_io_3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace es_io_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("output.txt");
+            string[] linee = File.ReadAllLines("password.txt");
+            string kiave;
+            List<string> kiavi = new List<string>();
+            List<string> password_singole = new List<string>();
+            List<int> pos = new List<int>();
+            List<int> top_5 = new List<int>();
+            for (int i = 0; i < linee.Length; i++)
+            {
+                for (int j = 0; j < linee[i].Length; j++)
+                {
+                    if (linee[i][j] == '|')
+                    {
+                        kiave = linee[i].Substring(j + 1);
+                        kiavi.Add(kiave);
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < kiavi.Count; i++)
+            {
+                if (!ripetuta(kiavi, pos,i))
+                {
+                    password_singole[i] = kiavi[i];
+                }
+            }
+            int[] 
+
+
+            sw.Close();
+            Console.ReadKey();
+        }
+
+        static bool ripetuta(List<string> kiavi, List<int> pos, int indiacatore)
+        {
+            for (int i = 0; i < kiavi.Count; i++)
+            {
+                if (kiavi[indiacatore] == kiavi[i])
+                {
+                    pos.Add(indiacatore);
+                    return true;
+                }
+            }
+            return false;
+
+        }
+    }
+}
